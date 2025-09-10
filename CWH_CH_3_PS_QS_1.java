32a57c456fc8dabb5da812058f7ef5d72139bb3a
Anshu Chauhan <anshuchauhan780045@gmail.com>
2025-09-10 18:16:47 +0530

    STRINGS IN JAVA
    In this program we convert the lower case of string  in java using string function.

---
+++ CWH_CH_3_PS_QS_1.java
@@ -0,0 +1,8 @@
+package com.coreJava;
+
+public class CWH_CH_3_PS_QS_1 {
+    public static void main(String[] args) {
+        String  name="ANSHU CHAUHAN";
+        System.out.println(name.toLowerCase());
+    }
+}
